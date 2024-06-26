f5825d968a59d5049adc4fffd6a59f7100a5ae82
chaitanya-kumar33 <chaitanyakumar1922@gmail.com>
2024-06-26 09:31:09 +0530

    Add files via upload

--- app/plugins/greet/__init__.py
+++ app/plugins/greet/__init__.py
@@ -5,4 +5,10 @@ from app.commands import Command
 class GreetCommand(Command):
     def execute(self):
         logging.info("Hello, World!")
+
+        mylist_tuple = (1,2,3,4)
+        mylist = [1,2,3,4]
+
+        
+
         print("Hello, World!")
